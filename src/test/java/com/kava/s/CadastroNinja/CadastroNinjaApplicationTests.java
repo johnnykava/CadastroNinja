960b992712d5e5972a7ce4ebe0671306c22e861b
Johnny Kava <johnnylk2003@hotmail.com>
2025-07-16 11:39:43 -0300

    first commit

---
+++ src/test/java/com/kava/s/CadastroNinja/CadastroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kava.s.CadastroNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
