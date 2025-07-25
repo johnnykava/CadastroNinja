3b08507853b132c33b044e212d6ff4b6c4177516
Johnny Kava <johnnylk2003@hotmail.com>
2025-07-25 11:13:44 -0300

    feat: Cria o mock do Controller do Ninja

--- src/main/java/com/kava/s/CadastroNinja/ninja/controller/NinjaController.java
+++ src/main/java/com/kava/s/CadastroNinja/ninja/controller/NinjaController.java
@@ -1,15 +1,43 @@
 package com.kava.s.CadastroNinja.ninja.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira rota";
+    }
+
+    //Adicionar Ninja
+    @PostMapping("/criar")
+    public String adicionarNinja(){
+        return "Adiciona ninja";
+    }
+
+    //Mostrar todos os ninjas
+    @GetMapping("/listar")
+    public String mostrarTodosNinjas(){
+        return "Mostra todos os Ninjas";
+    }
+
+    //Mostrar ninja por id
+    @GetMapping("/listarID")
+    public String mostrarNinjaPorId(){
+        return "Mostra ninja por ID";
+    }
+
+    //Alterar ninja
+    @PutMapping("/alteraID")
+    public String alterarNinja(){
+        return "Altera ninja";
+    }
+
+    //Deletar Ninja
+    @DeleteMapping("/deletarID")
+    public String deletaNinja(){
+        return "Deleta ninja";
     }
 }
