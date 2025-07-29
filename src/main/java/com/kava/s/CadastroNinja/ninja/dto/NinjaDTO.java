a2bbdb82fe1f12d1610c010f1db343333c261c76
Johnny Kava <johnnylk2003@hotmail.com>
2025-07-29 15:07:50 -0300

    chore: Inclui o DTO e mapeia a entidade ninja

--- src/main/java/com/kava/s/CadastroNinja/ninja/dto/NinjaDTO.java
+++ src/main/java/com/kava/s/CadastroNinja/ninja/dto/NinjaDTO.java
@@ -1,4 +1,19 @@
 package com.kava.s.CadastroNinja.ninja.dto;
 
+import com.kava.s.CadastroNinja.missao.models.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissaoModel missoes;
 }
